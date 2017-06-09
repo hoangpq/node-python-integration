b996a8f3c921b9fd67a941c558309f0889b376e1
hoangpp <pquochoang2007@gmail.com>
2017-06-10 05:43:01 +0700

    fix(python vm): create vm object factory

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const log = require('util').log;
-const pyVM = require('./build/Release/pyvm').vm;
+const pyvm = require('./build/Release/pyvm');
 
-// init vm
-const sys = pyVM().import('sys');
-const __builtin__ = pyVM().import('__builtin__');
+const __builtin__ = pyvm.import('__builtin__');
+const sys = pyvm.import('sys');
 
 // call function
+log(`Result pow(2, 3) is ${__builtin__.call('pow', 2, 3)}`);
 log(`Result of sys.getrecursionlimit() is ${sys.call('getrecursionlimit')}`);
 log(`Result of sys.getrefcount(0) is ${sys.call('getrefcount', 0)}`);
-log(`Result pow(2, 3) is ${__builtin__.call('pow', 2, 3)}`);
+
+console.log(__builtin__.list());
